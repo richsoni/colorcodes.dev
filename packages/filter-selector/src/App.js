a7734a79ddeed32da1e0d0aade59b84f45ec4cae
Rich Soni <rich@richsoni.com>
2019-11-27 01:12:13 -0500

    where im at

--- packages/filter-selector/src/App.js
+++ packages/filter-selector/src/App.js
@@ -1,18 +1,36 @@
 import React, {useState} from 'react';
 import logo from './logo.svg';
+import {min} from 'lodash';
 import './App.css';
-import { Button, Checkbox, Label, List, Input, Header, Container, Segment, Form } from 'semantic-ui-react';
+import { Checkbox, Select, Card, Item, Menu, Icon, Sidebar, Button, Label, List, Input, Header, Container, Segment, Form } from 'semantic-ui-react';
 import x11 from "@colorcodes/x11";
 import Color from "color";
+import Logo from './Logo';
 
 const RELATIONALS = {
   ABOVE: 'ABOVE',
   BELOW: 'BELOW'
 }
 
+const options = [
+  { key: 'all', text: 'All', value: 'all' },
+  { key: 'articles', text: 'Articles', value: 'articles' },
+  { key: 'products', text: 'Products', value: 'products' },
+]
+
 const Relational = ({params, onChange, relational}) => {
   return <Button active={relational === params.relational} onClick={() => onChange({relational: RELATIONALS[relational]})} >{relational}</Button>
 }
+
+const Filter = ({title, checked, onChange}) => {
+  return (<Segment>
+    <Header as='h3'>
+      {title}&nbsp;&nbsp;
+      <Checkbox checked={checked} onChange={() => onChange({[title]: !checked})} toggle label={checked ? 'ON' : 'OFF'} />
+    </Header>
+  </Segment>)
+}
+
 const ColorFilter = ({params, color, onChange = () => {}}) => {
    const changeHandler = (newParams) => {
      onChange({
@@ -21,8 +39,11 @@ const ColorFilter = ({params, color, onChange = () => {}}) => {
      })
    }
 
-   return (<>
-       <Button label={{color: color, content: params.value}} content={color} color={color} />
+   return (<Card>
+       <ColoredCardContent color={color} />
+       <Input max={255} min={0} onChange={(e) => (changeHandler({value: e.target.value}))} labelPosition='left' type='number' value={params.value} action>
+         <input />
+       </Input>
        <div>
          <input value={params.value} onChange={(e) => (changeHandler({value: e.target.value}))}
          type='range'
@@ -35,7 +56,7 @@ const ColorFilter = ({params, color, onChange = () => {}}) => {
          <Button.Or />
          <Relational onChange={changeHandler} params={params} relational={RELATIONALS.ABOVE} />
        </Button.Group>
-   </>);
+   </Card>);
 }
 
 const filterColor = (rgb, params) => {
@@ -53,45 +74,100 @@ const initialColorState = {
   relational: RELATIONALS.ABOVE,
 }
 
+const rgbSum = (color) => {
+  return color.rgb[0] + color.rgb[1] + color.rgb[2];
+}
+
+const fgColor = (color) => {
+ return Color(color).isDark() ? "white" : "black";
+}
+
+const ColoredCardContent = ({color}) => {
+  return (<Card.Content
+     textAlign="center"
+     style={{
+       background: color,
+       padding: "2em 0em",
+       color: fgColor(color),
+     }}
+  >{color}</Card.Content>)
+
+}
+
+const ColorButton = ({hueFilter, color}) => {
+  const [hue, setHue] = hueFilter;
+  return (<Button
+     onClick={() => hueFilter[1](Color(color).hsl().color[0])}
+     active={hueFilter[0] === color}
+     inverted
+     color={color}
+  >{color.toUpperCase()}</Button>);
+}
+
+const getDominantRGB = (rgb) => {
+  return 0
+}
+
+const getSortValue = (c) => {
+  // https://stackoverflow.com/questions/3014402/sorting-a-list-of-colors-in-one-dimension
+  const color = Color(c.name)
+  const hsl = color.hsl()
+  return hsl.color[2] * 5 + hsl.color[2] * 2 + hsl.color[0]
+}
 function App() {
-  const [redFilter, setRedFilter] = useState(initialColorState);
-  const [greenFilter, setGreenFilter] = useState(initialColorState);
-  const [blueFilter, setBlueFilter] = useState(initialColorState);
+  const hueFilter = useState(-1)
+  const [hue, setHue] = hueFilter;
+  const [filters, setFilters] = useState({
+    'Grayscale': true,
+  })
 
   const colors = x11
-    .filter((color => filterColor(color.rgb[0], redFilter)))
-    .filter((color => filterColor(color.rgb[1], greenFilter)))
-    .filter((color => filterColor(color.rgb[2], blueFilter)))
-  return (
+    .filter((c) => {
+      if(hue < 0) return true
+      if(c.name === 'black') return false;
+      const color = Color(c.name)
+      const distance = Math.abs(hue - color.hsl().color[0])
+      return distance < 60;
+    })
+    .sort((a,b) => getSortValue(a) - getSortValue(b))
+
+  return (<>
+    <Menu size='massive'>
+      <Menu.Item style={{backgroundColor: 'mintcream'}}><Logo /></Menu.Item>
+    </Menu>
     <Container>
        <Segment>
         <Header as='h1'>Filters</Header>
-        <div className='rgb-grid'>
-          <ColorFilter color='red' params={redFilter} onChange={setRedFilter} />
-          <ColorFilter color='green' params={greenFilter} onChange={setGreenFilter} />
-          <ColorFilter color='blue' params={blueFilter} onChange={setBlueFilter} />
-        </div>
+        <Filter checked={filters['Grayscale']} title='Grayscale' onChange={(state) => setFilters({...state})} />
+        <Segment>
+          <Header as='h3'>Dominant Color &nbsp;&nbsp;<Checkbox checked={hue > -1} onChange={() => setHue(hue > -1 ? -1 : 'red')} toggle label={hue > -1 ? 'ON' : 'OFF'} /></Header>
+          <ColorButton color='red' hueFilter={hueFilter} />
+          <ColorButton color='orange' hueFilter={hueFilter} />
+          <ColorButton color='yellow' hueFilter={hueFilter} />
+          <ColorButton color='green' hueFilter={hueFilter} />
+          <ColorButton color='blue' hueFilter={hueFilter} />
+          <ColorButton color='pink' hueFilter={hueFilter} />
+        </Segment>
        </Segment>
        <Segment>
          <Header as='h1'>Colors</Header>
          <Header as='h4'>{colors.length} of {x11.length} shown</Header>
-         {colors.map((color) => {
-           const fgColor = Color(color.hex).isDark() ? "white" : "black";
-           return (<Segment
-             key={color.name}
-             style={{
-               background: color.hex,
-               padding: "2em 0em",
-               color: fgColor,
-             }}
-           >
-            <Container textAlign="center" text>
-               {color.name}
-            </Container>
-           </Segment>
-         )})}
+         <Card.Group >
+           {colors.map((color) => (<Card
+               key={color.name}
+             >
+              <ColoredCardContent color={color.name} />
+              <Card.Content extra>
+                <Label color='red'>{color.rgb[0]}</Label>
+                <Label color='green'>{color.rgb[1]}</Label>
+                <Label color='blue'>{color.rgb[2]}</Label>
+              </Card.Content>
+             </Card>
+           ))}
+        </Card.Group>
       </Segment>
     </Container>
+    </>
   );
 }
 
---
+++ packages/filter-selector/src/Logo.js
@@ -0,0 +1,64 @@
+import React from 'react';
+
+export default (props) => {
+  return (<div style={{
+    fontFamily: "'Luckiest Guy', cursive",
+  }}>
+      <span style={{
+        color: 'gold',
+      }}>c</span>
+
+      <span style={{
+        color: 'orange',
+      }}>o</span>
+
+      <span style={{
+        color: 'tomato',
+      }}>l</span>
+
+      <span style={{
+        color: 'orchid',
+      }}>o</span>
+
+      <span style={{
+        color: 'royalblue',
+      }}>r</span>
+
+      <span style={{
+        color: 'mediumaquamarine',
+      }}>c</span>
+
+      <span style={{
+        color: 'lightgreen',
+      }}>o</span>
+
+      <span style={{
+        color: 'goldenrod',
+      }}>d</span>
+
+      <span style={{
+        color: 'sandybrown',
+      }}>e</span>
+
+      <span style={{
+        color: 'salmon',
+      }}>s</span>
+
+      <span style={{
+        color: 'blueviolet',
+      }}>.</span>
+
+      <span style={{
+        color: 'deepskyblue',
+      }}>d</span>
+
+      <span style={{
+        color: 'cadetblue',
+      }}>e</span>
+
+      <span style={{
+        color: 'darkseagreen',
+      }}>v</span>
+    </div>
+  )
+}
