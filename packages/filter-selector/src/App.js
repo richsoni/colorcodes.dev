cbf545df836883f8c6208427f2f0c2ca1c77a59e
Rich Soni <rich@richsoni.com>
2019-12-13 01:01:02 -0500

    feat: fix Hue Range Picker

--- packages/filter-selector/src/App.js
+++ packages/filter-selector/src/App.js
@@ -6,6 +6,7 @@ import Color from "color";
 import Logo from './Logo';
 import ColorTable from './ColorTable';
 import {Rnd} from 'react-rnd';
+import { Range, getTrackBackground } from 'react-range';
 
 const FilterCheckbox = ({value, onChange}) => (
   <Checkbox checked={value} onChange={(e, {checked}) => onChange(checked)} toggle label={value ? 'ON' : 'OFF'} />
@@ -34,25 +35,61 @@ const filterConfig = {
     initialValue: false,
   },
 
-  hsl: { title: "HSL Center",
+  hue: { title: "Hue Range",
     filter: ({color, value}) => {
-      const colorHSL = hsl(color.name)
-      const valueHSL = hsl(value)
-      const hDistance = distance(colorHSL.h, valueHSL.h);
-      const sDistance = distance(colorHSL.s, valueHSL.s);
-      const lDistance = distance(colorHSL.l, valueHSL.l);
-      const hThresh = square(Math.log(valueHSL.h + 1) + 1)
-      console.log(hThresh)
-      switch(value) {
-        default: return hDistance < hThresh && sDistance < 90 && lDistance < 50;
-      }
+      const hue = hsl(color.name).h
+      const min = value[0]
+      return hue >= value[0] && hue <= value[1]
     },
-    control: ({value, onChange}) => (<Button.Group>
-        {['red','orange','yellow','green','blue','violet'].map((color) => (
-          <Button color={color} onClick={() => onChange(color)}>{color}</Button>
-        ))}
-      </Button.Group>),
-    initialValue: 'red',
+
+    control: ({value, onChange}) => {
+      const MAX = 351;
+      return ( <Segment
+        style={{
+          background: 'linear-gradient(to right, #f00 0%, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, #f00 100%)',
+          border: 0,
+          padding: 0,
+          width:'100%',
+          height: '3em',
+          display: 'flex',
+          flexFlow: 'column nowrap',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+    >
+      <Range
+        step={1}
+        min={0}
+        max={MAX}
+        values={value}
+        onChange={values => onChange(values)}
+        renderTrack={({ props, children }) => (
+          <div
+          {...props}
+            style={{
+              height: '6px',
+              background: getTrackBackground({values: value, min: 0, max: MAX, colors: ['transparent', 'black', 'transparent']}),
+              width: '100%',
+            }}
+          >
+            {children}
+          </div>
+        )}
+        renderThumb={({ props }) => (
+          <div
+            {...props}
+            style={{
+              ...props.style,
+              height: '100%',
+              width: '1em',
+              backgroundColor: 'black'
+            }}
+          />
+        )}
+      />
+    </Segment>)},
+
+    initialValue: [50,75],
   },
 }
 
@@ -61,6 +98,10 @@ const getSortValue = (c) => {
   const {h,s,l} = hsl(c.name)
   return l * 5 + s * 2 + h
 }
+
+const Handle = ({translateX}) => (
+  <Label circular color='black' empty style={{transform: `translate(${translateX}, 50%)`}} />
+)
 
 function App() {
   const [hslFilter, setHslFilter] = useState({h: 0, s: 0, l: 0})
@@ -87,7 +128,7 @@ function App() {
       );
     }, x11)
     .sort((a,b) => getSortValue(a) - getSortValue(b))
-
+      
   return (<>
     <Menu size='massive'>
       <Menu.Item style={{backgroundColor: 'mintcream'}}><Logo /></Menu.Item>
@@ -95,39 +136,12 @@ function App() {
     <Container>
        <Segment>
         <Header as='h1'>Filters</Header>
-        <Segment
-            style={{
-              background: 'linear-gradient(to right, #f00 0%, #ff0 17%, #0f0 33%, #0ff 50%, #00f 67%, #f0f 83%, #f00 100%)',
-              position: 'relative'
-            }}
-    >
-       <div style={{width: '100%', height: '100%', position: 'relative'}}>
-        <Rnd
-          dragAxis='x'
-          bounds='parent'
-          size={{ width: rndState.width,  height: rndState.height }}
-          enableResizing={{ top:false, right:true, bottom:false, left:true, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
-          position={{ x: rndState.x, y: 0}}
-          onDragStop={(e, d) => { setRndState({...rndState, x: d.x, y: d.y}) }}
-          onResizeStop={(e, direction, ref, delta, position) => {
-            setRndState({
-                width: ref.style.width, 
-                height: ref.style.height,
-                ...position
-            })
-          }}
-        >
-          001
-        </Rnd>
-    </div>
-        </Segment>
         <List>
           {Object
             .keys(filterConfig)
             .map((key) => {
               const config = filterConfig[key];
               const Control = config.control;
-              console.log(config.title, filters[key])
               return (
                 <List.Item key={key}>
                   <List.Header>{config.title}</List.Header>
@@ -142,7 +156,7 @@ function App() {
           </List>
        </Segment>
        <Segment>
-         <Header as='h1'>Colors {Math.max(...x11.map((x) => hsl(x.name).h))}</Header>
+         <Header as='h1'>Colors</Header>
          <Header as='h4'>{colors.length} of {x11.length} shown</Header>
          <ColorTable colors={colors} />
       </Segment>
